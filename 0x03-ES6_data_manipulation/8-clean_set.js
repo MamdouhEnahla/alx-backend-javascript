eb4bf85aa0345f76ef5e7a50a516d615dfbcaed8
MamdouhEnahla <mamdouh57118@gmail.com>
2024-07-31 02:12:14 +0300

    clean-set...

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,21 @@
+/**
+ * Joins a set of strings with a dash after stripping the strings of
+ */
+
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+  const resultArray = [];
+
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        resultArray.push(valueSubStr);
+      }
+    }
+  }
+  return resultArray.join('-');
+}
